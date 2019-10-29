af5de33dc2f87ea88951fb308902f6140a4a8613
trafalmejo <trafalmejo@gmail.com>
2019-10-29 18:48:27 -0400

    Updates

---
+++ precache-manifest.051b62ffc7960a7e5f41406b99d6ea04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a3f25d5daab94a1a0c014cf41b6b5fd",
+    "url": "/howrandom/index.html"
+  },
+  {
+    "revision": "a82685c69adcf194d4b4",
+    "url": "/howrandom/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1e70f3f689ee2c0c8eb1",
+    "url": "/howrandom/static/js/2.8bef6599.chunk.js"
+  },
+  {
+    "revision": "a82685c69adcf194d4b4",
+    "url": "/howrandom/static/js/main.6cc493d6.chunk.js"
+  },
+  {
+    "revision": "03fe0615b349f13df4b4",
+    "url": "/howrandom/static/js/runtime-main.fb2b7363.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/howrandom/static/media/logo.25bf045c.svg"
+  }
+]);
